39cdffeb9d695c10aea7f1b8e643478d7b338d16
elligoro <elligoro@gmail.com>
2021-08-12 23:53:04 +0300

    1_add_db added context and crypto class

---
+++ EntityServer/DbContext/DbContext.cs
@@ -0,0 +1,44 @@
+﻿using EntityServer.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityServer
+{
+    public class UserAuthDbContext:DbContext
+    {
+        public DbSet<UserCreds> UsersCreds{ get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            base.OnConfiguring(builder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // userName
+            builder.Entity<UserCreds>()
+                   .HasIndex(uc => uc.Username)
+                   .IsUnique();
+
+            // seed
+            builder.Entity<UserCreds>().HasData(new UserCreds { Username = "simon", Password = CryptoService.HashSHA256("12345donteattheyellowsnow123493")});
+        }
+    }
+
+    public class UserCreds
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+
+
+}
---
+++ EntityServer/Services/CryptoService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EntityServer.Services
+{
+    public static class CryptoService
+    {
+        public static string HashSHA256(string msg)
+        {
+            using(var hash256 = SHA256.Create())
+            return GetHash(hash256,msg);
+        } 
+        public static string GetHash(HashAlgorithm hashAlgorithm, string msg)
+        {
+            var dataBytes = Encoding.UTF8.GetBytes(msg);
+            byte[] dataHash = hashAlgorithm.ComputeHash(dataBytes);
+
+            var hexArr = dataHash.Select(b => b.ToString("x2"));
+            return string.Join("", hexArr);
+        }
+        public static bool VerifyHash(HashAlgorithm hashAlgorithm, string hash, string msg)
+        {
+            var hashedMsg = GetHash(hashAlgorithm, msg);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            return comparer.Compare(hashedMsg, msg) == 0;
+        }
+    }
+}
